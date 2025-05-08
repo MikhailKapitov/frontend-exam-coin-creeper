e9e8d6d617a2c93391ee5d3099e485d86a8ad439
MikhailKapitov <mkapitov4@gmail.com>
2025-05-08 21:04:45 +0500

    Fixed the dumb submodule bug.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+export default function App() {
+
+  return (
+    <>
+      <header className="header">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod<br></br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,<br></br>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo<br></br>consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse<br></br>cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non<br></br>proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </header>
+
+      <content id="content">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod<br></br>tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,<br></br>quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo<br></br>consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse<br></br>cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non<br></br>proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </content>
+    </>
+  );
+}
